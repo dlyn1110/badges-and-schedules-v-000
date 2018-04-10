e434418142f50c852555601e14ca2c4dbcd48eca
Dani Lyn <dlyn1110@gmail.com>
2018-04-10 14:13:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  badge_maker = "Arel"
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+ attendees.collect do | name|
+   badge_maker(name)
+end
+end
