d26e140f44e8e2e9e74c4187740cda667745701e
Dani Lyn <dlyn1110@gmail.com>
2018-04-10 14:35:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,13 @@ end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do | attendees, index |
+  attendees.each_with_index do | attendees, room |
    "Hello, #{attendees}! You'll be assigned to room #{index+1}!"
+ end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do | speaker|
+    puts speaker
  end
 end
